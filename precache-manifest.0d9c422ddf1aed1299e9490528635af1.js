095f524c0ada380b78325e15717ae1d27c3f548e
Thomas Horacek <horacek.thomas@gmail.com>
2018-10-13 15:49:52 -0600

    Updates

---
+++ precache-manifest.0d9c422ddf1aed1299e9490528635af1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a3c0fd1fd6699c0ae04",
+    "url": "/mlb-standings-react/static/js/runtime~main.8a3c0fd1.js"
+  },
+  {
+    "revision": "e841c09119b3cd8384f5",
+    "url": "/mlb-standings-react/static/js/main.e841c091.chunk.js"
+  },
+  {
+    "revision": "9f32f6b6c399190a73a6",
+    "url": "/mlb-standings-react/static/js/1.9f32f6b6.chunk.js"
+  },
+  {
+    "revision": "e841c09119b3cd8384f5",
+    "url": "/mlb-standings-react/static/css/main.9df45433.chunk.css"
+  },
+  {
+    "revision": "9f32f6b6c399190a73a6",
+    "url": "/mlb-standings-react/static/css/1.db48be62.chunk.css"
+  },
+  {
+    "revision": "58ac7b22f6e0819949b73adbb6b398f4",
+    "url": "/mlb-standings-react/index.html"
+  }
+];
